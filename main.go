bb733bb2a98fa0340ed1283c85aeb1a835c282bd
luispdm <17044119+luispdm@users.noreply.github.com>
2023-10-10 16:23:38 +0200

    calculate last block; endless goroutine; flags

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"log/slog"
 	"math/big"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -11,13 +15,63 @@ import (
 )
 
 func main() {
-	cli, err := ethclient.Dial("https://eth.llamarpc.com")
+	rpc := flag.String("rpc", "https://eth.llamarpc.com", "URL of the RPC node of an evm-compatible blockchain")
+	// 0xBC4... is the Bored Ape ERC721 Ethereum contract
+	contractAddress := flag.String("contract", "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D", "Web3 address of the smart contract")
+	startingBlock := flag.Uint64("starting_block", 18288287, "Initial block where the scanning process should start from")
+	blocksRange := flag.Uint("block_range", 100, "Amount of blocks the scanner processes")
+	blocksMargin := flag.Uint("block_margin", 70, "Number of blocks to assume finality")
+	flag.Parse()
+
+	cli, err := ethclient.Dial(*rpc)
 	if err != nil {
 		slog.Error("error instantiating eth client", "msg", err.Error())
 		os.Exit(1)
 	}
 
-	// Bored Ape ERC721 Ethereum contract
-	contract := common.HexToAddress("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D")
-	scanner.ScanEvents(cli, contract, big.NewInt(18288287), nil)
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill, syscall.SIGTERM)
+	defer stop()
+
+	contract := common.HexToAddress(*contractAddress)
+	if *startingBlock == 0 {
+		*startingBlock, err = getL1LatestBlock(cli, ctx)
+		if err != nil {
+			logger.Error("error retrieving the latest block", "msg", err.Error())
+			os.Exit(1)
+		}
+	}
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			l1LatestBlock, err := getL1LatestBlock(cli, ctx)
+			if err != nil {
+				logger.Error("error retrieving the latest block", "msg", err.Error())
+				break
+			}
+			lastBlock := calculateLastBlock(*startingBlock, l1LatestBlock, *blocksRange, *blocksMargin)
+			if lastBlock < *startingBlock {
+				logger.Debug("last calculated block is behind starting block, continue...")
+				break
+			}
+			scanner.ScanEvents(cli, contract, big.NewInt(int64(*startingBlock)), big.NewInt(int64(lastBlock)))
+			*startingBlock = lastBlock + 1
+		}
+	}
+}
+
+func getL1LatestBlock(cli *ethclient.Client, ctx context.Context) (uint64, error) {
+	lastBlock, err := cli.BlockNumber(ctx)
+	if err != nil {
+		return 0, err
+	}
+	return lastBlock, nil
+}
+
+func calculateLastBlock(startingBlock, l1LatestBlock uint64, blocksRange, blocksMargin uint) uint64 {
+	return min(startingBlock+uint64(blocksRange), l1LatestBlock-uint64(blocksMargin))
 }
