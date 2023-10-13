cb8e3970ccb9c5dceae3cda47d5ee4765c12106e
Jürgen Kudla <kudlaj@gmail.com>
2023-10-13 07:57:46 +0200

    PR fix

--- scripts/install-helmfile.sh
+++ scripts/install-helmfile.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Install helm-diff plugin
-helm plugin install https://github.com/databus23/helm-diff --version v3.6.0
+helm plugin install https://github.com/databus23/helm-diff --version v3.8.1
 
 # Set URL for latest release of helmfile
 URL=https://github.com/helmfile/helmfile/releases/download/v0.157.0/helmfile_0.157.0_linux_amd64.tar.gz
