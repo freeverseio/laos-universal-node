4a4b2e53961b4af80e51dd9d8a76313ae9d4440c
Jürgen Kudla <kudlaj@gmail.com>
2023-10-18 09:55:31 +0200

    removing assert library

--- internal/rpc/erc721/call_test.go
+++ internal/rpc/erc721/call_test.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -20,9 +18,8 @@ func TestProcessCallUnallowedMethod(t *testing.T) {
 	chainID := uint64(1)
 
 	_, err := ProcessCall(data, to, mockEthClient, contractAddr, chainID)
-	assert.Error(t, err)
-	if assert.Error(t, err) {
-		assert.Equal(t, "unallowed method: 0x12345678", err.Error())
+	if err == nil || err.Error() != "unallowed method: 0x12345678" {
+		t.Errorf("Expected error: unallowed method: 0x12345678, got: %v", err)
 	}
 }
 
@@ -37,16 +34,24 @@ func TestProcessCall(t *testing.T) {
 	chainID := uint64(1)
 
 	res, err := ProcessCall(data, to, mockClient, contractAddr, chainID)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
 
 	// Assert that the returned result matches the expected one
-	assert.Equal(t, ExpectedData, hexutil.Encode(res))
+	if hexutil.Encode(res) != ExpectedData {
+		t.Errorf("Expected result: %v, got: %v", ExpectedData, hexutil.Encode(res))
+	}
 
 	// Additional validation for the decoded data (if necessary)
 	decodedData, err := hexutil.Decode(hexutil.Encode(res))
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatalf("Expected no error during decode, got: %v", err)
+	}
+
 	// Extract the actual string from the ABI-encoded data
 	actualStr := string(decodedData[64 : 64+4]) // Assuming that "www0" is always 4 bytes long
-
-	assert.Equal(t, "www0", actualStr)
+	if actualStr != "www0" {
+		t.Errorf("Expected string: www0, got: %v", actualStr)
+	}
 }
