00139c824eae0809cc4461fe0ea3654e4eba5643
luispdm <17044119+luispdm@users.noreply.github.com>
2023-10-26 11:55:17 +0200

    add comment in store regarding block

--- internal/scan/store.go
+++ internal/scan/store.go
@@ -14,7 +14,9 @@ import (
 
 type ERC721BridgelessContract struct {
 	Address common.Address `json:"address"`
-	Block   uint64         `json:"block"` // TODO how to use this when scanning new events?
+	// this will be renamed "currentBlock" and stored in the DB with the related contract
+	// this way the scan can continue scanning that contract from that block
+	Block   uint64         `json:"block"`
 	BaseURI string         `json:"base_uri"`
 }
 
