0ba7637a819e6d4303901485d4665f4885aba116
Jürgen Kudla <kudlaj@gmail.com>
2023-11-03 12:52:30 +0100

    adding calldata to parse data

---
+++ internal/platform/rpc/erc721/calldata.go
@@ -0,0 +1,116 @@
+package erc721
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/freeverseio/laos-universal-node/internal/platform/blockchain/contract"
+)
+
+// calldata represents the data for an ERC721 function call.
+type CallData []byte
+
+// NewCalldata creates a new calldata instance from a hexadecimal string.
+func NewCallData(s string) (CallData, error) {
+	b, err := hexutil.Decode(s)
+	if err != nil {
+		return CallData{}, err
+	}
+	return b, nil
+}
+
+// erc721method represents the supported ERC721 methods.
+type Erc721method int
+
+const ShortAddressLength = 4
+
+const (
+	OwnerOf Erc721method = iota
+	BalanceOf
+	TokenURI
+	URI
+	SupportsInterface
+	Name
+	ContractURI
+	Decimals
+	Symbol
+)
+
+var methodSigs = map[string]Erc721method{
+	hexutil.Encode(crypto.Keccak256([]byte("ownerOf(uint256)"))[:ShortAddressLength]):          OwnerOf,
+	hexutil.Encode(crypto.Keccak256([]byte("balanceOf(address)"))[:ShortAddressLength]):        BalanceOf,
+	hexutil.Encode(crypto.Keccak256([]byte("tokenURI(uint256)"))[:ShortAddressLength]):         TokenURI,
+	hexutil.Encode(crypto.Keccak256([]byte("supportsInterface(bytes4)"))[:ShortAddressLength]): SupportsInterface,
+	hexutil.Encode(crypto.Keccak256([]byte("name()"))[:ShortAddressLength]):                    Name,
+	hexutil.Encode(crypto.Keccak256([]byte("contractURI()"))[:ShortAddressLength]):             ContractURI,
+	hexutil.Encode(crypto.Keccak256([]byte("decimals()"))[:ShortAddressLength]):                Decimals,
+	hexutil.Encode(crypto.Keccak256([]byte("symbol()"))[:ShortAddressLength]):                  Symbol,
+}
+
+// Method returns the ERC721 method invoked by the calldata.
+func (b CallData) Method() (Erc721method, error) {
+	sig, err := b.methodSignature()
+	if err != nil {
+		return 0, err
+	}
+
+	if method, exists := methodSigs[sig]; exists {
+		return method, nil
+	}
+
+	return 0, fmt.Errorf("unallowed method: %s", sig)
+}
+
+// methodSignature returns the method signature of the calldata.
+func (b CallData) methodSignature() (string, error) {
+	if len(b) < ShortAddressLength {
+		return "", fmt.Errorf("invalid call data, incomplete method signature (%d bytes < 4)", len(b))
+	}
+	return hexutil.Encode(b[:ShortAddressLength]), nil
+}
+
+// GetParam returns the value of a specific parameter in the input arguments of the calldata.
+func (b CallData) GetParam(param string) (interface{}, error) {
+	inputArgs, err := b.getInputArgs()
+	if err != nil {
+		return nil, err
+	}
+	if inputArgs[param] == nil {
+		return nil, fmt.Errorf("param %s not found into the object %v", param, inputArgs)
+	}
+	return inputArgs[param], nil
+}
+
+// getInputArgs returns a map of input arguments extracted from the calldata.
+func (b CallData) getInputArgs() (map[string]interface{}, error) {
+	sig, err := b.methodSignature()
+	if err != nil {
+		return nil, err
+	}
+
+	argdata := b[ShortAddressLength:]
+	if len(argdata)%32 != 0 {
+		return nil, fmt.Errorf("invalid call data; lengsth should be a multiple of 32 bytes but was %d", len(argdata))
+	}
+	erc721Abi, err := abi.JSON(strings.NewReader(contract.Erc721universalMetaData.ABI))
+	if err != nil {
+		return nil, err
+	}
+	id, err := hexutil.Decode(sig)
+	if err != nil {
+		return nil, err
+	}
+	method, err := erc721Abi.MethodById(id)
+	if err != nil {
+		return nil, err
+	}
+	inputArgs := map[string]interface{}{}
+	err = method.Inputs.UnpackIntoMap(inputArgs, argdata)
+	if err != nil {
+		return nil, err
+	}
+	return inputArgs, nil
+}
---
+++ internal/platform/rpc/erc721/calldata_test.go
@@ -0,0 +1,148 @@
+package erc721_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/freeverseio/laos-universal-node/internal/platform/rpc/erc721"
+)
+
+func TestNewCalldata(t *testing.T) {
+	tests := []struct {
+		input         string
+		expected      erc721.CallData
+		expectedError string
+	}{
+		{
+			input:         "0x1234",
+			expected:      erc721.CallData{0x12, 0x34},
+			expectedError: "",
+		},
+		{
+			input:         "invalid",
+			expected:      erc721.CallData{},
+			expectedError: "hex string without 0x prefix",
+		},
+	}
+
+	for _, test := range tests {
+		output, err := erc721.NewCallData(test.input)
+
+		if !slicesEqual(output, test.expected) {
+			t.Errorf("Expected: %v, got: %v", test.expected, output)
+		}
+
+		if test.expectedError == "" {
+			if err != nil {
+				t.Errorf("Expected no error, got: %v", err)
+			}
+		} else {
+			if err == nil || err.Error() != test.expectedError {
+				t.Errorf("Expected error: %v, got: %v", test.expectedError, err)
+			}
+		}
+	}
+}
+
+func TestMethod(t *testing.T) {
+	tests := []struct {
+		input    erc721.CallData
+		expected erc721.Erc721method
+		err      error
+	}{
+		{
+			input:    hexutil.MustDecode("0x6352211e"),
+			expected: erc721.OwnerOf,
+			err:      nil,
+		},
+		{
+			input:    hexutil.MustDecode("0x70a08231"),
+			expected: erc721.BalanceOf,
+			err:      nil,
+		},
+		{
+			input:    hexutil.MustDecode("0xc87b56dd"),
+			expected: erc721.TokenURI,
+			err:      nil,
+		},
+		{
+			input:    hexutil.MustDecode("0x01ffc9a7"),
+			expected: erc721.SupportsInterface,
+			err:      nil,
+		},
+		{
+			input:    hexutil.MustDecode("0x06fdde03"),
+			expected: erc721.Name,
+			err:      nil,
+		},
+		{
+			input:    hexutil.MustDecode("0x313ce567"),
+			expected: erc721.Decimals,
+			err:      nil,
+		},
+		{
+			input:    erc721.CallData{0x00, 0x00, 0x00},
+			expected: 0,
+			err:      fmt.Errorf("invalid call data, incomplete method signature (3 bytes < 4)"),
+		},
+		{
+			input:    erc721.CallData{0x12, 0x34, 0x56, 0x78},
+			expected: 0,
+			err:      fmt.Errorf("unallowed method: 0x12345678"),
+		},
+	}
+
+	for _, test := range tests {
+		output, err := test.input.Method()
+		if output != test.expected {
+			t.Errorf("Expected: %v, got: %v", test.expected, output)
+		}
+		if err == nil && test.err != nil {
+			if err.Error() != test.err.Error() {
+				t.Errorf("Expected error: %v, got: %v", test.err, err)
+			}
+		}
+	}
+}
+
+func TestGetParam(t *testing.T) {
+	// This is a bit more complex since it requires correct ABI encoding.
+	// For the sake of example, I'll provide a simple framework.
+	tests := []struct {
+		input    erc721.CallData
+		param    string
+		expected interface{}
+		err      error
+	}{
+		{
+			input:    hexutil.MustDecode("0x70a08231000000000000000000000000bd7931f025ecf360b21e1ab92ec34b49084bca5b"),
+			param:    "owner",
+			expected: common.HexToAddress("0xbD7931f025ecF360b21E1aB92ec34b49084bcA5B"),
+			err:      nil,
+		},
+	}
+
+	for _, test := range tests {
+		output, err := test.input.GetParam(test.param)
+		if output != test.expected {
+			t.Errorf("Expected: %v, got: %v", test.expected, output)
+		}
+		if err != test.err {
+			t.Errorf("Expected error: %v, got: %v", test.err, err)
+		}
+	}
+}
+
+func slicesEqual(a, b erc721.CallData) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
