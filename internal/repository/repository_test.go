e9918cbd2d246b00bd20315bc96b85803507872b
Jürgen Kudla <kudlaj@gmail.com>
2023-11-13 08:15:26 +0100

    adding more tests for repository

--- internal/repository/repository_test.go
+++ internal/repository/repository_test.go
@@ -85,3 +85,171 @@ func TestStoreERC721UniversalContracts(t *testing.T) {
 		}
 	})
 }
+
+func TestGetAllERC721UniversalContracts(t *testing.T) {
+	t.Parallel()
+	t.Run("should execute GetAllERC721UniversalContracts without error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockStorage := mock.NewMockStorage(mockCtrl)
+		service := repository.New(mockStorage)
+
+		keys := [][]byte{
+			[]byte("contract_0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"),
+			[]byte("contract_0x0"),
+		}
+
+		mockStorage.EXPECT().GetKeysWithPrefix([]byte("contract_")).Return(keys, nil)
+
+		contracts, err := service.GetAllERC721UniversalContracts()
+		if err != nil {
+			t.Fatalf("got error %s, expecting no error", err.Error())
+		}
+
+		if len(contracts) != len(keys) {
+			t.Fatalf("got %d contracts, expecting %d contracts", len(contracts), len(keys))
+		}
+
+		for i := 0; i < len(contracts); i++ {
+			if string(keys[i]) != contracts[i] {
+				t.Fatalf("got contract %s, expecting %s", contracts[i], string(keys[i]))
+			}
+		}
+	})
+
+	t.Run("should execute GetAllERC721UniversalContracts and handle an error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockStorage := mock.NewMockStorage(mockCtrl)
+		service := repository.New(mockStorage)
+
+		errExpected := fmt.Errorf("error")
+		mockStorage.EXPECT().GetKeysWithPrefix([]byte("contract_")).Return([][]byte{}, errExpected)
+
+		_, err := service.GetAllERC721UniversalContracts()
+		if err == nil {
+			t.Fatalf("got no error, expecting en error %s", errExpected.Error())
+		}
+	})
+}
+
+func TestGetChainID(t *testing.T) {
+	t.Parallel()
+	t.Run("should execute GetChainID without error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockStorage := mock.NewMockStorage(mockCtrl)
+		service := repository.New(mockStorage)
+
+		mockStorage.EXPECT().Get([]byte("chain_id")).Return([]byte("1"), nil)
+
+		chainID, err := service.GetChainID()
+		if err != nil {
+			t.Fatalf("got error %s, expecting no error", err.Error())
+		}
+
+		if chainID != "1" {
+			t.Fatalf("got chainID %s, expecting 1", chainID)
+		}
+	})
+
+	t.Run("should execute GetChainID and handle an error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockStorage := mock.NewMockStorage(mockCtrl)
+		service := repository.New(mockStorage)
+
+		errExpected := fmt.Errorf("error")
+		mockStorage.EXPECT().Get([]byte("chain_id")).Return([]byte(""), errExpected)
+
+		_, err := service.GetChainID()
+		if err == nil {
+			t.Fatalf("got no error, expecting en error %s", errExpected.Error())
+		}
+	})
+}
+
+func TestGetCurrentBlock(t *testing.T) {
+	t.Parallel()
+	t.Run("should execute GetCurrentBlock without error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockStorage := mock.NewMockStorage(mockCtrl)
+		service := repository.New(mockStorage)
+
+		mockStorage.EXPECT().Get([]byte("current_block")).Return([]byte("1"), nil)
+
+		currentBlock, err := service.GetCurrentBlock()
+		if err != nil {
+			t.Fatalf("got error %s, expecting no error", err.Error())
+		}
+
+		if currentBlock != "1" {
+			t.Fatalf("got currentBlock %s, expecting 1", currentBlock)
+		}
+	})
+}
+
+func TestStoreCurrentBlock(t *testing.T) {
+	t.Parallel()
+	t.Run("should execute StoreCurrentBlock without error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockStorage := mock.NewMockStorage(mockCtrl)
+		service := repository.New(mockStorage)
+		mockStorage.EXPECT().Set([]byte("current_block"), []byte("2")).Return(nil)
+
+		err := service.SetCurrentBlock("2")
+		if err != nil {
+			t.Fatalf("got error %s, expecting no error", err.Error())
+		}
+	})
+}
+
+func TestHasERC721UniversalContract(t *testing.T) {
+	t.Parallel()
+	t.Run("should execute HasERC721UniversalContract without error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+		mockStorage := mock.NewMockStorage(mockCtrl)
+		service := repository.New(mockStorage)
+		mockStorage.EXPECT().Get([]byte("contract_0x0")).Return([]byte("http://baseuri2.com"), nil)
+
+		has, err := service.HasERC721UniversalContract("0x0")
+		if err != nil {
+			t.Fatalf("got error %s, expecting no error", err.Error())
+		}
+
+		if !has {
+			t.Fatalf("got false, expecting true")
+		}
+	})
+
+	t.Run("should execute HasERC721UniversalContract and handle an error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+		mockStorage := mock.NewMockStorage(mockCtrl)
+		service := repository.New(mockStorage)
+		errExpected := fmt.Errorf("error")
+		mockStorage.EXPECT().Get([]byte("contract_0x0")).Return([]byte(""), errExpected)
+
+		_, err := service.HasERC721UniversalContract("0x0")
+		if err == nil {
+			t.Fatalf("got no error, expecting en error %s", errExpected.Error())
+		}
+	})
+}
