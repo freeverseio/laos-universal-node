3cda727429c097dfbbfcad3d63618178f1d59bbb
zorjak <zorjak@gmail.com>
2023-11-26 01:04:40 +0100

    added some tests for get and set CurrentOwnershipBlock

---
+++ internal/state/block/ownership/block_test.go
@@ -0,0 +1,78 @@
+package ownership_test
+
+import (
+	"testing"
+
+	"github.com/freeverseio/laos-universal-node/internal/platform/storage/mock"
+	v1 "github.com/freeverseio/laos-universal-node/internal/state/v1"
+	"go.uber.org/mock/gomock"
+)
+
+func TestGetCurrentOwnershipBlock(t *testing.T) {
+	t.Parallel()
+	t.Run("should execute GetCurrentOwnershipBlock without error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockStorage := mock.NewMockService(mockCtrl)
+		mockStorageTransaction := mock.NewMockTx(mockCtrl)
+		mockStorage.EXPECT().NewTransaction().Return(mockStorageTransaction)
+
+		stateService := v1.NewStateService(mockStorage)
+		tx := stateService.NewTransaction()
+
+		mockStorageTransaction.EXPECT().Get([]byte("ownership_current_block")).Return([]byte("1"), nil)
+
+		currentBlock, err := tx.GetCurrentOwnershipBlock()
+		if err != nil {
+			t.Fatalf("got error %s, expecting no error", err.Error())
+		}
+
+		if currentBlock != 1 {
+			t.Fatalf("got currentBlock %d, expecting 1", currentBlock)
+		}
+	})
+}
+
+func TestSetCurrentOwnershipBlock(t *testing.T) {
+	t.Parallel()
+	t.Run("should execute GetCurrentOwnershipBlock without error", func(t *testing.T) {
+		t.Parallel()
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mockStorage := mock.NewMockService(mockCtrl)
+		mockStorageTransaction := mock.NewMockTx(mockCtrl)
+		mockStorage.EXPECT().NewTransaction().Return(mockStorageTransaction)
+
+		stateService := v1.NewStateService(mockStorage)
+		tx := stateService.NewTransaction()
+
+		mockStorageTransaction.EXPECT().Set([]byte("ownership_current_block"), []byte("1")).Return(nil)
+
+		err := tx.SetCurrentOwnershipBlock(uint64(1))
+		if err != nil {
+			t.Fatalf("got error %s, expecting no error", err.Error())
+		}
+
+	})
+}
+
+// func TestStoreCurrentBlock(t *testing.T) {
+// 	t.Parallel()
+// 	t.Run("should execute StoreCurrentBlock without error", func(t *testing.T) {
+// 		t.Parallel()
+// 		mockCtrl := gomock.NewController(t)
+// 		defer mockCtrl.Finish()
+
+// 		mockStorage := mock.NewMockService(mockCtrl)
+// 		service := repository.New(mockStorage)
+// 		mockStorage.EXPECT().Set([]byte("current_block"), []byte("2")).Return(nil)
+
+// 		err := service.SetCurrentBlock("2")
+// 		if err != nil {
+// 			t.Fatalf("got error %s, expecting no error", err.Error())
+// 		}
+// 	})
+// }
