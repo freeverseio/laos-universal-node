dc47a943521220e8a4dfdfc462792086b64c96a0
luispdm <17044119+luispdm@users.noreply.github.com>
2023-12-11 16:11:36 +0100

    remove unnecessary white spaces

--- internal/state/v1/state.go
+++ internal/state/v1/state.go
@@ -72,7 +72,7 @@ func (t *tx) CreateTreesForContract(contract common.Address) (
 	enumeratedTotalTree enumeratedtotal.Tree,
 	err error,
 ) {
-	slog.Debug("creating trees for contract ", "contract", contract.String())
+	slog.Debug("creating trees for contract", "contract", contract.String())
 
 	ownershipTree, err = ownership.NewTree(contract, t.tx)
 	if err != nil {
@@ -108,7 +108,7 @@ func (t *tx) SetTreesForContract(
 
 // OwnerOf returns the owner of the token
 func (t *tx) OwnerOf(contract common.Address, tokenId *big.Int) (common.Address, error) {
-	slog.Debug("OwnerOf ", "contract", contract.String(), "tokenId", tokenId.String())
+	slog.Debug("OwnerOf", "contract", contract.String(), "tokenId", tokenId.String())
 	ownershipTree, ok := t.ownershipTrees[contract]
 	if !ok {
 		return common.Address{}, fmt.Errorf("contract %s does not exist", contract.String())
@@ -118,7 +118,7 @@ func (t *tx) OwnerOf(contract common.Address, tokenId *big.Int) (common.Address,
 
 // BalanceOf returns the balance of the owner
 func (t *tx) BalanceOf(contract, owner common.Address) (*big.Int, error) {
-	slog.Debug("BalanceOf ", "contract", contract.String(), "owner", owner.String())
+	slog.Debug("BalanceOf", "contract", contract.String(), "owner", owner.String())
 	enumeratedTree, ok := t.enumeratedTrees[contract]
 	if !ok {
 		return big.NewInt(0), fmt.Errorf("contract %s does not exist", contract.String())
@@ -134,7 +134,7 @@ func (t *tx) BalanceOf(contract, owner common.Address) (*big.Int, error) {
 
 // TokenOfOwnerByIndex returns the token of the owner by index
 func (t *tx) TokenOfOwnerByIndex(contract, owner common.Address, idx int) (*big.Int, error) {
-	slog.Debug("TokenOfOwnerByIndex ", "contract", contract.String(), "owner", owner.String(), "idx", idx)
+	slog.Debug("TokenOfOwnerByIndex", "contract", contract.String(), "owner", owner.String(), "idx", idx)
 	enumeratedTree, ok := t.enumeratedTrees[contract]
 	if !ok {
 		return big.NewInt(0), fmt.Errorf("contract %s does not exist", contract.String())
@@ -152,7 +152,7 @@ func (t *tx) TokenOfOwnerByIndex(contract, owner common.Address, idx int) (*big.
 
 // Transfer transfers ownership of the token. From, To, and TokenID are set in event
 func (t *tx) Transfer(contract common.Address, eventTransfer *model.ERC721Transfer) error {
-	slog.Debug("Transfer ", "contract",
+	slog.Debug("Transfer", "contract",
 		contract.String(),
 		"From", eventTransfer.From.String(),
 		"To", eventTransfer.To.String(), "tokenId",
@@ -222,7 +222,7 @@ func (t *tx) Transfer(contract common.Address, eventTransfer *model.ERC721Transf
 
 // Mint creates a new token
 func (t *tx) Mint(contract common.Address, mintEvent *model.MintedWithExternalURI) error {
-	slog.Debug("Mint ", "contract", contract.String(), "tokenId", mintEvent.TokenId.String())
+	slog.Debug("Mint", "contract", contract.String(), "tokenId", mintEvent.TokenId.String())
 	enumeratedTotalTree, ok := t.enumeratedTotalTrees[contract]
 	if !ok {
 		return fmt.Errorf("contract %s does not exist", contract.String())
@@ -263,7 +263,7 @@ func (t *tx) Mint(contract common.Address, mintEvent *model.MintedWithExternalUR
 
 // TotalSupply returns the total number of tokens in the contract
 func (t *tx) TotalSupply(contract common.Address) (int64, error) {
-	slog.Debug("TotalSupply ", "contract", contract.String())
+	slog.Debug("TotalSupply", "contract", contract.String())
 	enumeratedTotalTree, ok := t.enumeratedTotalTrees[contract]
 	if !ok {
 		return 0, fmt.Errorf("contract %s does not exist", contract.String())
@@ -274,7 +274,7 @@ func (t *tx) TotalSupply(contract common.Address) (int64, error) {
 
 // TokenByIndex returns the token at the index
 func (t *tx) TokenByIndex(contract common.Address, idx int) (*big.Int, error) {
-	slog.Debug("TokenByIndex ", "contract", contract.String(), "idx", idx)
+	slog.Debug("TokenByIndex", "contract", contract.String(), "idx", idx)
 	enumeratedTotalTree, ok := t.enumeratedTotalTrees[contract]
 	if !ok {
 		return big.NewInt(0), fmt.Errorf("contract %s does not exist", contract.String())
@@ -285,7 +285,7 @@ func (t *tx) TokenByIndex(contract common.Address, idx int) (*big.Int, error) {
 
 // TagRoot tags roots for all 3 merkle trees at the same block
 func (t *tx) TagRoot(contract common.Address, blockNumber int64) error {
-	slog.Debug("TagRoot ", "contract", contract.String(), "blockNumber", strconv.FormatInt(blockNumber, 10))
+	slog.Debug("TagRoot", "contract", contract.String(), "blockNumber", strconv.FormatInt(blockNumber, 10))
 	enumeratedTree, ok := t.enumeratedTrees[contract]
 	if !ok {
 		return fmt.Errorf("contract %s does not exist", contract.String())
@@ -315,7 +315,7 @@ func (t *tx) TagRoot(contract common.Address, blockNumber int64) error {
 }
 
 func (t *tx) GetLastTaggedBlock(contract common.Address) (int64, error) {
-	slog.Debug("GetLastTaggedBlock ", "contract", contract.String())
+	slog.Debug("GetLastTaggedBlock", "contract", contract.String())
 	enumeratedTree, ok := t.enumeratedTrees[contract]
 	if !ok {
 		return 0, fmt.Errorf("contract %s does not exist", contract.String())
@@ -325,7 +325,7 @@ func (t *tx) GetLastTaggedBlock(contract common.Address) (int64, error) {
 }
 
 func (t *tx) DeleteRootTag(contract common.Address, blockNumber int64) error {
-	slog.Debug("DeleteRootTag ", "contract", contract.String(), "blockNumber", strconv.FormatInt(blockNumber, 10))
+	slog.Debug("DeleteRootTag", "contract", contract.String(), "blockNumber", strconv.FormatInt(blockNumber, 10))
 	enumeratedTree, ok := t.enumeratedTrees[contract]
 	if !ok {
 		return fmt.Errorf("contract %s does not exist", contract.String())
@@ -363,7 +363,7 @@ func (t *tx) Checkout(contract common.Address, blockNumber int64) error {
 	// probably the easiest and cleanest solution would be to write separate functions for creating transactions
 	// NewTransactionForRead and NewTransactionForWrite instead of NewTransaction
 
-	slog.Debug("Checkout ", "contract", contract.String(), "blockNumber", strconv.FormatInt(blockNumber, 10))
+	slog.Debug("Checkout", "contract", contract.String(), "blockNumber", strconv.FormatInt(blockNumber, 10))
 	enumeratedTree, ok := t.enumeratedTrees[contract]
 	if !ok {
 		return fmt.Errorf("contract %s does not exist", contract.String())
