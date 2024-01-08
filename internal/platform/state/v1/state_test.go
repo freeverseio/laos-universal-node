82c1af4811e3097a953f49fef8870f7d7e38a2e6
luispdm <17044119+luispdm@users.noreply.github.com>
2024-01-09 00:34:17 +0100

    add successful loadmerkletrees test

--- internal/platform/state/v1/state_test.go
+++ internal/platform/state/v1/state_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/freeverseio/laos-universal-node/internal/platform/state"
 	v1 "github.com/freeverseio/laos-universal-node/internal/platform/state/v1"
 	"github.com/freeverseio/laos-universal-node/internal/platform/storage/memory"
 )
@@ -13,9 +14,7 @@ func TestLoadMerkleTree(t *testing.T) {
 	t.Parallel()
 	t.Run("fails when contract is 0x0", func(t *testing.T) {
 		t.Parallel()
-		mem := memory.New()
-		stateService := v1.NewStateService(mem)
-		tx := stateService.NewTransaction()
+		tx := createTransaction()
 		expectedErr := fmt.Sprintf("contract address is " + common.Address{}.String())
 
 		err := tx.LoadMerkleTrees(common.HexToAddress("0x0"))
@@ -26,4 +25,19 @@ func TestLoadMerkleTree(t *testing.T) {
 			t.Fatalf(`got error "%s", expected "%s"`, err.Error(), expectedErr)
 		}
 	})
+	t.Run("successfully loads merkle trees in memory", func(t *testing.T) {
+		t.Parallel()
+		tx := createTransaction()
+
+		err := tx.LoadMerkleTrees(common.HexToAddress("0x500"))
+		if err != nil {
+			t.Errorf(`got error "%v" when no error was expected`, err)
+		}
+	})
+}
+
+func createTransaction() state.Tx {
+	mem := memory.New()
+	stateService := v1.NewStateService(mem)
+	return stateService.NewTransaction()
 }
