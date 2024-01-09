67e6a98a755e51e799b92966c102b0585623cb8f
luispdm <17044119+luispdm@users.noreply.github.com>
2024-01-09 12:51:39 +0100

    add mint to bb test

--- internal/platform/state/v1/state_test.go
+++ internal/platform/state/v1/state_test.go
@@ -2,9 +2,11 @@ package v1_test
 
 import (
 	"fmt"
+	"math/big"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/freeverseio/laos-universal-node/internal/platform/model"
 	"github.com/freeverseio/laos-universal-node/internal/platform/state"
 	v1 "github.com/freeverseio/laos-universal-node/internal/platform/state/v1"
 	"github.com/freeverseio/laos-universal-node/internal/platform/storage/memory"
@@ -28,8 +30,24 @@ func TestLoadMerkleTrees(t *testing.T) {
 	t.Run("successfully loads merkle trees in memory", func(t *testing.T) {
 		t.Parallel()
 		tx := createTransaction()
+		contract := common.HexToAddress("0x500")
 
-		err := tx.LoadMerkleTrees(common.HexToAddress("0x500"))
+		err := tx.LoadMerkleTrees(contract)
+		if err != nil {
+			t.Errorf(`got error "%v" when no error was expected`, err)
+		}
+
+		// if tx.Mint works, we are sure that the merkle trees are correctly loaded in memory
+		// as Mint accesses all merkle trees
+		mintEvent := model.MintedWithExternalURI{
+			Slot:        big.NewInt(1),
+			To:          common.HexToAddress("0x3"),
+			TokenURI:    "tokenURI",
+			TokenId:     big.NewInt(1),
+			BlockNumber: 100,
+			Timestamp:   1000,
+		}
+		err = tx.Mint(contract, &mintEvent)
 		if err != nil {
 			t.Errorf(`got error "%v" when no error was expected`, err)
 		}
