600810b3a159068922f4d795accbbe44eb208ba0
zorjak <zorjak@gmail.com>
2024-01-31 23:00:02 +0100

    adding tests to improve coverage

--- internal/platform/state/sync/evolution/sync_test.go
+++ internal/platform/state/sync/evolution/sync_test.go
@@ -5,9 +5,12 @@ import (
 	"encoding/gob"
 	"testing"
 
+	"github.com/dgraph-io/badger/v4"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/freeverseio/laos-universal-node/internal/platform/model"
+	"github.com/freeverseio/laos-universal-node/internal/platform/state"
 	v1 "github.com/freeverseio/laos-universal-node/internal/platform/state/v1"
+	badgerStorage "github.com/freeverseio/laos-universal-node/internal/platform/storage/badger"
 	"github.com/freeverseio/laos-universal-node/internal/platform/storage/mock"
 	"go.uber.org/mock/gomock"
 )
@@ -61,4 +64,51 @@ func TestSetGetLastEvoBlock(t *testing.T) {
 	if newBlock.Hash != block.Hash {
 		t.Fatalf("got block hash %s, expecting %s", newBlock.Hash.String(), block.Hash.String())
 	}
+}
+
+func TestSetGetNextNextEvoEventBlock(t *testing.T) {
+	t.Parallel()
+	t.Run("stores minted events", func(t *testing.T) {
+		t.Parallel()
+		db := createBadger(t)
+		tx, err := createBadgerTransaction(t, db)
+		if err != nil {
+			t.Errorf(`got error "%v" when no error was expected`, err)
+		}
+
+		err = tx.SetNextEvoEventBlock(common.HexToAddress("0x500").Hex(), 100)
+		if err != nil {
+			t.Errorf(`got error "%v" when no error was expected`, err)
+		}
+
+		block, err := tx.GetNextEvoEventBlock(common.HexToAddress("0x500").Hex(), 0)
+		if err != nil {
+			t.Errorf(`got error "%v" when no error was expected`, err)
+		}
+
+		if block != 100 {
+			t.Errorf(`got block %d when 100 was expected`, block)
+		}
+
+	})
+}
+
+func createBadgerTransaction(t *testing.T, db *badger.DB) (state.Tx, error) {
+	t.Helper()
+	badgerService := badgerStorage.NewService(db)
+	stateService := v1.NewStateService(badgerService)
+	return stateService.NewTransaction()
+}
+
+func createBadger(t *testing.T) *badger.DB {
+	t.Helper()
+	db, err := badger.Open(
+		badger.DefaultOptions("").
+			WithInMemory(true).
+			WithLoggingLevel(badger.ERROR).WithMemTableSize(1 << 30))
+	if err != nil {
+		t.Fatalf("error initializing storage: %v", err)
+	}
+
+	return db
 }
