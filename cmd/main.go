569c18465ef9878d2a40004634da4df83365d29c
Jürgen Kudla <kudlaj@gmail.com>
2024-02-08 11:38:58 +0100

    adding evoRpcUrl

--- cmd/main.go
+++ cmd/main.go
@@ -190,7 +190,7 @@ func run() error {
 		}
 		addr := fmt.Sprintf("0.0.0.0:%v", c.Port)
 		slog.Info("starting RPC server", "listen_address", addr)
-		return rpcServer.ListenAndServe(ctx, c.Rpc, addr, c.EvoRpc, stateService)
+		return rpcServer.ListenAndServe(ctx, c.Rpc, c.EvoRpc, addr, stateService)
 	})
 
 	if err := group.Wait(); err != nil {
