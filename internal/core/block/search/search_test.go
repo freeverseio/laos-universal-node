6c7cc7ee55cd159e2352e940edcf846dd9e9d0ae
luispdm <17044119+luispdm@users.noreply.github.com>
2024-02-14 14:46:31 +0100

    reassign variable in parallel test

--- internal/core/block/search/search_test.go
+++ internal/core/block/search/search_test.go
@@ -137,6 +137,7 @@ func TestGetBlockByTimestamp(t *testing.T) {
 	}
 
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			ctrl := gomock.NewController(t)
