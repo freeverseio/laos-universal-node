d4f1be2c7c9529224194e2939796ee76e078eeda
luispdm <17044119+luispdm@users.noreply.github.com>
2024-02-15 16:20:11 +0100

    GetOwnershipInitStartingBlock positive tests

--- internal/core/processor/shared_test.go
+++ internal/core/processor/shared_test.go
@@ -2,11 +2,12 @@ package shared_test
 
 import (
 	"context"
-	"errors"
+	"fmt"
 	"testing"
 
 	shared "github.com/freeverseio/laos-universal-node/internal/core/processor"
 	blockchainMock "github.com/freeverseio/laos-universal-node/internal/platform/blockchain/mock"
+	"github.com/freeverseio/laos-universal-node/internal/platform/model"
 	stateMock "github.com/freeverseio/laos-universal-node/internal/platform/state/mock"
 	"go.uber.org/mock/gomock"
 )
@@ -20,8 +21,8 @@ func TestGetLastBlock(t *testing.T) {
 			blockNumber uint64
 			expected    uint64
 		}{
-			{"returns the left side of the minimum", 20, 11},
-			{"returns the right side of the minimum", 10, 10},
+			{"should return the left side of the minimum", 20, 11},
+			{"should return the right side of the minimum", 10, 10},
 		}
 
 		for _, tt := range tests {
@@ -46,7 +47,7 @@ func TestGetLastBlock(t *testing.T) {
 			})
 		}
 	})
-	t.Run("handles an error from BlockNumber", func(t *testing.T) {
+	t.Run("should return the error from BlockNumber", func(t *testing.T) {
 		t.Parallel()
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
@@ -54,7 +55,7 @@ func TestGetLastBlock(t *testing.T) {
 		mockClient, mockStateService := getMocks(ctrl)
 
 		blockHelper := shared.NewBlockHelper(mockClient, mockStateService, 10, 0, 1)
-		expectedErr := errors.New("an error occurred")
+		expectedErr := fmt.Errorf("an error occurred")
 		mockClient.EXPECT().BlockNumber(context.Background()).Return(uint64(0), expectedErr)
 
 		_, err := blockHelper.GetLastBlock(context.Background(), 1)
@@ -65,6 +66,66 @@ func TestGetLastBlock(t *testing.T) {
 			t.Errorf("got error message '%s' while '%s' was expected", err.Error(), expectedErr.Error())
 		}
 	})
+}
+
+func TestGetOwnershipInitStartingBlock(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockClient, mockStateService := getMocks(ctrl)
+
+	tests := []struct {
+		name                  string
+		startingBlockData     model.Block
+		userStartingBlock     uint64
+		chainLatestBlock      uint64
+		expectedStartingBlock uint64
+		blockNumberTimes      int
+	}{
+		{
+			name:                  "should use starting block from storage",
+			startingBlockData:     model.Block{Number: 10},
+			userStartingBlock:     0,
+			chainLatestBlock:      0,
+			expectedStartingBlock: 11,
+			blockNumberTimes:      0,
+		},
+		{
+			name:                  "should use user provided starting block",
+			startingBlockData:     model.Block{Number: 0},
+			userStartingBlock:     20,
+			chainLatestBlock:      0,
+			expectedStartingBlock: 20,
+			blockNumberTimes:      0,
+		},
+		{
+			name:                  "should use latest block from chain when no starting block provided",
+			startingBlockData:     model.Block{Number: 0},
+			userStartingBlock:     0,
+			chainLatestBlock:      30,
+			expectedStartingBlock: 30,
+			blockNumberTimes:      1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := stateMock.NewMockTx(ctrl)
+			mockStateService.EXPECT().NewTransaction().Return(tx, nil)
+			tx.EXPECT().Discard().Times(1)
+			tx.EXPECT().GetLastOwnershipBlock().Return(tt.startingBlockData, nil).Times(1)
+			mockClient.EXPECT().BlockNumber(context.Background()).Return(tt.chainLatestBlock, nil).Times(tt.blockNumberTimes)
+
+			helper := shared.NewBlockHelper(mockClient, mockStateService, 100, 10, tt.userStartingBlock)
+			actualStartingBlock, err := helper.GetOwnershipInitStartingBlock(context.Background())
+			if err != nil {
+				t.Errorf("got error '%v' while no error was expected", err)
+			}
+			if actualStartingBlock != tt.expectedStartingBlock {
+				t.Errorf("got %d, expected starting block %d", tt.expectedStartingBlock, actualStartingBlock)
+			}
+		})
+	}
 }
 
 func getMocks(ctrl *gomock.Controller) (*blockchainMock.MockEthClient, *stateMock.MockService) {
